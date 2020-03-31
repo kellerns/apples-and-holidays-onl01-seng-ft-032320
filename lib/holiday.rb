8a3d488c6ff35b62e9ecb1bd6ed57118d95bc548
Nicholas Keller <nic.keller328@gmail.com>
2020-03-31 20:27:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
 
     holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize
+    puts season.to_s.capitalize + ":"
 
       holiday.each do |holiday, supplies|
         holiday_array = holiday.to_s.split("_")
